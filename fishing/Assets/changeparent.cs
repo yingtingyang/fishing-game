fe68c29342369eece8d0f10d6c24e821378ed12e
yingtingyang <yangy389@newschool.edu>
2016-09-29 16:16:21 -0400

    inital commit

---
+++ fishing/Assets/changeparent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeparent : MonoBehaviour {
+    public Transform thingToFollow;
+    public float speed = 1;
+    // Use this for initialization
+    void Start()
+    {
+        	transform.parent = GameObject.Find ("Fish").transform;
+        // this is how to change the hierarchy dynamically in Unity 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(thingToFollow);
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
---
+++ fishing/Assets/fishcatch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishcatch : MonoBehaviour {
+    public float lure;
+
+    fishmovement fm;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+
+    }
+     void OnTriggerEnter(Collider otherGOCollider)
+    {
+        if (otherGOCollider.gameObject.tag == "lure")
+        {
+
+
+            fm = GetComponent<fishmovement>();
+            fm.enabled = false;
+            transform.parent = otherGOCollider.transform;
+        }
+    }
+}
